9a56a95539680038fe0129261be6a7913ae4df36
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-02-19 12:05:23 +0530

    Added: Multiple flips of coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,10 +2,18 @@
 
 head=1
 tail=0
-random=$(( RANDOM%2 ))
-if (( $random == $head ))
-then
-echo "$random - Winner is Head "
-else
-echo "$random - Winner is Tail"
-fi
+headcount=0
+tailcount=0
+read -p "Enter the number of coin flips : " n
+for (( i=0; i<n; i++ ))
+do
+	random=$(( $RANDOM % 2 ))
+	if(( $random == 1))
+	then
+		(( headcount++ ))
+	else
+   	(( tailcount++ ))
+	fi
+done
+echo "Heads won $headcount times"
+echo "Tails won $tailcount times"
