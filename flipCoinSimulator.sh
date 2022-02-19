d693e84cf1c709f8f0db26393123a40a3e5e4cc6
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-02-19 14:09:12 +0530

    Added: Flip 21 times or tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,19 +1,32 @@
+
 #!/bin/bash
 
 head=1
 tail=0
 headcount=0
 tailcount=0
-read -p "Enter the number of coin flips : " n
-for (( i=0; i<n; i++ ))
+while [ $headcount -lt 21  ] && [ $tailcount -lt 21  ]
 do
 	random=$(( $RANDOM % 2 ))
-	if(( $random == 1))
+	if(( $random == $head))
 	then
 		(( headcount++ ))
 	else
    	(( tailcount++ ))
 	fi
 done
-echo "Heads won $headcount times"
-echo "Tails won $tailcount times"
+
+if [ $headcount == 21  ]
+then
+	difH=$(( $headcount - $tailcount ))
+	echo Head count is $headcount
+	echo Head won by $difH Tail counts.
+elif [ $tailcount == 21  ]
+then
+	difT=$(( $tailcount - $headcount ))
+	echo Tail count is $tailcount
+	echo Tail won by $difT Head counts.
+elif [ $headcount == 21 ] && [ $tailcount == 21 ]
+then
+	echo It is a tie
+fi
